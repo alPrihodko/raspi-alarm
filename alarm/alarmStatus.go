babbdb31aff488387e61afbb8adb75b35e560af8
alPrihodko <alPrihodko@gmail.com>
2016-06-16 01:37:23 +0300

    arm/disarm

--- alarm/alarmStatus.go
+++ alarm/alarmStatus.go
@@ -3,6 +3,9 @@ package alarm
 import (
 	"log"
 	"os"
+	"os/exec"
+	"strings"
+	"sync"
 )
 
 const armedFile = "/var/lock/raspi-alarm-armed.lock"
@@ -45,6 +48,10 @@ func (q alarm) Arm() error {
 		return err
 	}
 	newFile.Close()
+
+	wg := new(sync.WaitGroup)
+	go exeCmd("/etc/init.s/uv4l_uvc start 046d:081b", wg)
+	wg.Wait()
 	Alarm.Armed = true
 	return nil
 }
@@ -57,6 +64,22 @@ func (q alarm) Disarm() error {
 	if err != nil {
 		return err
 	}
+	wg := new(sync.WaitGroup)
+	go exeCmd("/etc/init.s/uv4l_uvc stop", wg)
+	wg.Wait()
+
 	Alarm.Armed = false
 	return nil
+}
+
+func exeCmd(cmd string, wg *sync.WaitGroup) {
+	log.Println(cmd)
+	parts := strings.Fields(cmd)
+	out, err := exec.Command(parts[0], parts[1]).Output()
+	if err != nil {
+		log.Println("error occured")
+		log.Println(err.Error())
+	}
+	log.Println(out)
+	wg.Done()
 }
