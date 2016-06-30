ef2a9b7455788183196d39d5f2b29c904d43f12b
alPrihodko <alPrihodko@gmail.com>
2016-07-01 02:03:18 +0300

    sms sending

---
+++ alarm_sms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+XML='<?xml version="1.0" encoding="UTF-8"?>
+<request>
+    <auth>
+        <login>380939760324</login>
+        <password>fktrc-fk</password>
+    </auth>
+    <message>
+          <from>Alarm System</from>
+          <text>Move detected</text>
+          <recipient>380939760324</recipient>
+    </message>
+</request>'
+
+curl -vvv -d "$XML" http://letsads.com/api
