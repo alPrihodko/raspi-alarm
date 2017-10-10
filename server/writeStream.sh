47861c0c7a6a0cf467664f8ac8ea3973a4ff4c81
alPrihodko <alPrihodko@gmail.com>
2017-10-11 01:36:53 +0300

    migrate to vlc on the server

--- server/writeStream.sh
+++ server/writeStream.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cameraSet.sh
+#cameraSet.sh
 
 while :
 do
     #for i in {1..200}
     #do
     timestamp=$( date +%F_%T )
-    ffmpeg -i http://user:fktrc-fk@109.86.198.152:8090/stream/video.mjpeg -fs 1000000000 -vcodec flv "/home/al/motion/home-$timestamp.flv"
-
+    #ffmpeg -i http://user:fktrc-fk@109.86.198.152:8090/stream/video.mjpeg -fs 1000000000 -vcodec flv "/home/al/motion/home-$timestamp.flv"
+    cvlc http://109.86.198.152:8090 --run-time 1800 --sout="#std{access=file,mux=ogg,dst=/home/al/motion/home-$timestamp.ogg}" vlc://quit
+    
     if [[ $? -ne 0 ]]; then
         echo 'Error reading data, timeout 10 secs'
         sleep 5
