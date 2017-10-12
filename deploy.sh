7445254ffae1bb300a260f088d7facbe2b44a0d9
al <alprihodko@gmail.com>
2017-10-12 14:52:51 +0300

    improve dep script

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,19 @@
 HOST=192.168.1.46
 deploy=pi\@"$HOST":/usr/local/bin
 
-scp alarm.sh $deploy
-scp alarm_sms.sh $deploy
-scp arm.sh $deploy
-scp disarm.sh $deploy
+scp alarm.sh $deploy || exit 1
+scp alarm_sms.sh $deploy || exit 1
+scp arm.sh $deploy || exit 1
+scp disarm.sh $deploy || exit 1
 
 export GOOS=linux
 export GOARCH=arm
 export GOARM=7
-go build || exit 1
 
-ssh pi\@$HOST "sudo service raspi-alarm stop"
-scp raspi-alarm $deploy
-ssh pi\@$HOST "sudo service raspi-alarm start"
-echo "Success"
+if [ "$1" -eq "all" ]; then
+    go build || exit 1
+    ssh pi\@$HOST "sudo service raspi-alarm stop" || exit 1
+    scp raspi-alarm $deploy || exit 1
+    ssh pi\@$HOST "sudo service raspi-alarm start" || exit 1
+    echo "Success"
+fi
