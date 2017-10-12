f7fd8dc16c72a079f0f9dd9d835b58c383f91702
al <alprihodko@gmail.com>
2017-10-12 14:20:10 +0300

    fixing services

--- server/writeStream.sh
+++ server/writeStream.sh
@@ -8,7 +8,7 @@ do
     #do
     timestamp=$( date +%F_%T )
     #ffmpeg -i http://user:fktrc-fk@109.86.198.152:8090/stream/video.mjpeg -fs 1000000000 -vcodec flv "/home/al/motion/home-$timestamp.flv"
-    cvlc http://109.86.198.152:8090 --run-time 1800 --sout="#std{access=file,mux=ogg,dst=/home/al/motion/home-$timestamp.ogg}" vlc://quit
+    cvlc http://109.86.198.152:8090 --run-time 7200 --sout="#std{access=file,mux=ogg,dst=/home/al/motion/home-$timestamp.ogg}" vlc://quit
     
     if [[ $? -ne 0 ]]; then
         echo 'Error reading data, timeout 10 secs'
